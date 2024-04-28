4415ead55e86683eced2a75eab57eacc8df6d2a8
Alexis <AlexDdevs@gmail.com>
2024-04-28 12:16:21 +0200

    Create a component for the carrousel and call it

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -5,8 +5,8 @@ import transition from '../utils/transition'
 const Destination = () => {
     return (
         <main className='main-destination'>
-            <p className='pick'><span>01</span>Pick your destination</p>
             <section>
+                <h1 className='pick'><span>01</span>Pick your destination</h1>
                 <StarCarrousel />
             </section>
         </main>
