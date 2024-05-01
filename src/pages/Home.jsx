2d05b43cf596b00d029050fd44fd8369d83feb12
Alexis <AlexDdevs@gmail.com>
2024-05-01 08:12:23 +0200

    The code is small so put it directly in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import HomeContent from '../components/home/HomeContent'
 import transition from '../utils/transition'
 
 const Home = () => {
     return (
-        <HomeContent />
+        <main className='main-home'>
+            <section>
+                <p className='travel'>So, you want to travel to</p>
+                <h1>Space</h1>
+                <p className='explication'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+            </section>
+            <div className="explore">Explore</div>
+        </main>
     )
 }
 
