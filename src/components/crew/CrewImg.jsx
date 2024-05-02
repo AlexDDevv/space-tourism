05aea19082cce645afafc26f8fc4fdbc002e431e
Alexis <AlexDdevs@gmail.com>
2024-05-02 13:38:46 +0200

    Add transition slider carrousel

--- src/components/crew/CrewImg.jsx
+++ src/components/crew/CrewImg.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function CrewImg({ img, name, display }) {
     return (
-        <img
-            src={img}
-            alt={`A crew member: ${name}`}
-            className={`crew-img ${display}`}
-        />
+        <motion.div 
+            className="crew-img-container"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2, ease: "easeInOut" }}
+        >
+            <img
+                src={img}
+                alt={`A crew member: ${name}`}
+                className={`crew-img ${display}`}
+            />
+        </motion.div>
     )
 }
