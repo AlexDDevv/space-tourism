d2c1eabb672cc9c92db75047b32cd93e8818b119
Alexis <AlexDdevs@gmail.com>
2024-05-02 08:33:12 +0200

    Call CrewCarrousel in Crew page

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import CrewCarrousel from '../components/crew/CrewCarrousel'
+import transition from '../utils/transition'
 
-export default function Crew() {
-  return (
-    <div>Crew</div>
-  )
+const Crew = () => {
+    return (
+        <main className='crew-main'>
+            <section className='section-crew'>
+                <h1 className='meet-crew title-page'><span>02</span>Meet your crew</h1>
+                <CrewCarrousel />
+            </section>
+        </main>
+    )
 }
+
+export default transition(Crew)
